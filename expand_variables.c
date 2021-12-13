49f933adea66372546c4a1a22fcebee11639f95e
Manuel Almeida <malmeida@c1r8s1.42lisboa.com>
2021-12-13 12:50:43 +0000

    working on expanding vars, very close but segfaulting

--- expand_variables.c
+++ expand_variables.c
@@ -6,11 +6,43 @@
 /*   By: malmeida <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 23:09:56 by malmeida          #+#    #+#             */
-/*   Updated: 2021/12/11 23:16:00 by malmeida         ###   ########.fr       */
+/*   Updated: 2021/12/13 12:50:02 by malmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	replace_var(char **str)
+{
+	int		i;
+	int		j;
+	int		len;
+	char	*copy;
+
+	copy = ft_strdup(*str);
+	len = ft_strlen(copy);
+	i = 0;
+	while (g_mini.env[i])
+	{
+		if (ft_strncmp(copy, g_mini.env[i], len - 1) == 0)
+			break;
+		i++;
+	}
+	if (ft_strncmp(copy, g_mini.env[i], len - 1) == 0)
+	{
+		*str = NULL;
+		j = 0;
+		while (g_mini.env[i][j] != '=')
+			j++;
+		*str = ft_substr(g_mini.env[i], j + 1, 100);
+	}
+	else
+	{
+		*str = NULL;
+		*str = malloc(sizeof(char));
+		*str[0] = '\0';
+	}
+}
 
 void	expand_variable(char **line, int start)
 {
@@ -25,7 +57,7 @@ void	expand_variable(char **line, int start)
 	while ((ft_isalpha(str.full[i]) || str.full[i] == '$') && str.full[i])
 		i++;
 	str.var = ft_substr(str.full, start, i - start);
-	// replace_var(&(str.var));
+	replace_var(&(str.var));
 	len = ft_strlen(str.full);
 	str.after = ft_substr(str.full, i, len - i);
 	printf("full str is: %s\n", str.full);
